67f5a1530d4b55e4d80b566070d8ef01d7c9f84b
Fabian <fab.parraguez@duocuc.cl>
2023-06-05 20:49:09 -0400

    Ahora los datos vienen del servidor, desde una lista creada en productos.py, se modifico el archivo views.py para que en la funcion mostrar_inicio retornara también un context para que en las plantilla index.html se hiciera un ciclo for que recorriera esta lista y fuera poniendo los elementos según lo que haya en la lista productos

--- Foodtech/productos.py
+++ Foodtech/productos.py
@@ -1,3 +1,23 @@
 productos = [
-    {}
+    {   'id':'1',
+        'img':'img/cook.webp',
+        'nombre': 'Lasaña',
+        'descripcion':'[Descripción]',
+        'precio':'[2000]',
+        
+    },
+    {   'id':'2',
+        'img':'img/cook2.jpg',
+        'nombre': 'Pasta Alfredo',
+        'descripcion':'[Descripción]',
+        'precio':'[1500]',
+        
+    },
+    {   'id':'3',
+        'img':'img/cook3.webp',
+        'nombre': 'Lasaña',
+        'descripcion':'[Descripción]',
+        'precio':'[3000]',
+        
+    },
 ]
--- Foodtech/views.py
+++ Foodtech/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 #from .views import 
+from .productos import productos
 def mostrar_inicio(request):
-    return render(request, 'index.html')
+    #Se traen los datos a la plantilla
+    context = {
+        'productos':productos
+    }
+    return render(request, 'index.html', context)
 
 def mostrar_registro(request):
     return render(request, 'registro.html')
