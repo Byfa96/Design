a44d49e6ae898dfd34dea8e6b6540817397c24cc
Fabian <fab.parraguez@duocuc.cl>
2023-06-08 06:43:26 -0400

    Se migro de base de datos

--- Foodtech/settings.py
+++ Foodtech/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from os.path import join
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'Foodtech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'software',
+        'USER': 'software',
+        'PASSWORD': 'Hola-1234',
+        'HOST': '144.22.51.31',
+        'PORT': '3306'
     }
 }
 
