2f64c778366782651174931fc89ebfbd6157a700
Fab <fab.parraguez@duocuc.cl>
2023-06-21 20:16:02 -0400

    xdd

--- tienda/models.py
+++ tienda/models.py
@@ -1,10 +1,17 @@
-from django.db.models import (Model, CharField, TextField, DecimalField, ImageField, ForeignKey, CASCADE)
-from django.contrib.auth.models import User
+from django.db.models import (Model, 
+                              CharField, 
+                              TextField, 
+                              DecimalField, 
+                              ImageField, 
+                              ForeignKey, 
+                              CASCADE)
 
+
+from django.contrib.auth.models import User
 
 class Producto(Model):
     nombre = CharField(max_length=100)
     descripcion = TextField()
     precio = DecimalField(max_digits=8, decimal_places=2)
     img = ImageField(upload_to="comida", blank=True)
-
+    usuario = ForeignKey(User, on_delete = CASCADE)
